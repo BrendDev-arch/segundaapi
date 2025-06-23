aee0fba3a36d0da6673d7c7a20eb06e4ec592d0e
BrendDev-arch <brendhonluccas@icloud.com>
2025-06-23 11:52:30 -0300

    Finalizando

---
+++ src/main/java/com/jkalango/webapi/AloController.java
@@ -0,0 +1,20 @@
+package com.jkalango.webapi;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+@RequestMapping("/alo")
+
+public class AloController {
+
+    //requisição e respostas (REQUEST E RESPONSA)
+    @GetMapping
+    public String aloMundo(){
+        return "Alô Mundo - JKalango!";
+
+    }
+
+}
